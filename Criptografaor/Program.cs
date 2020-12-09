1e36aa1e32d27c977584bb5e4ad20555e504c645
Douglasmantovani <almagedomchaotic19@gmail.com>
2020-12-09 11:55:17 -0300

    Exercicios

---
+++ Criptografaor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace opa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um texto");
+            var text= Console.ReadLine();
+            var cry=Cryptho(text);
+            Console.WriteLine(cry);
+        }
+
+        //Retorna um hash em SHA256
+        private static string Cryptho(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            System.Security.Cryptography.SHA256 MinhaHasher = System.Security.Cryptography.SHA256.Create();
+            byte[] hash = MinhaHasher.ComputeHash(bytes);
+            string NomeHash = string.Empty;
+            foreach (byte x in hash)
+            {
+                NomeHash += string.Format("{0:x2}", x);//Quanto maior o numero maior o tamanho da hash
+            }
+            return NomeHash;
+        }
+
+        //Retorna um hash criptografado em MD5
+        public static string Criptografador(string text)
+        {
+            MD5 myHash = MD5.Create();
+
+            byte[] Data = myHash.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+            StringBuilder stb = new StringBuilder();
+
+            for(int i = 0; i < Data.Length; i++)
+            {
+                stb.Append(Data[i].ToString("x3"));//Quanto maior o numero maior o tamanho da hash
+            }
+            return stb.ToString();
+        }
+    }
+}
---
+++ ManipulandoArquivos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace MovendoArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Move os arquivos de  local
+             var Origem="Origem/";
+             var Destino="Destino";
+             var NomeDoArquivo="/bigBoss.jpg";
+              var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), Origem+NomeDoArquivo);
+              var pathMove = Path.Combine(Directory.GetCurrentDirectory(),Destino+NomeDoArquivo);
+             System.IO.File.Move(pathToSave, pathMove, true);
+             
+
+            //Renomeia arquivos para dateTime(serve para evitar conflitos de nomes)
+             string teste=DateTime.Now.ToString("ddMMyyyyHHmmss");
+             //Pega o valor da extensao
+             string Extensao = Origem.Split('.')[1].Trim();
+             teste=teste+"."+Extensao;
+             Console.WriteLine(teste);
+              string sourceFile =Path.Combine(Directory.GetCurrentDirectory(), Origem+NomeDoArquivo);  
+             string source =Path.Combine(Directory.GetCurrentDirectory(), Origem);  
+             System.Console.WriteLine(source);
+              // Create a FileInfo  
+              System.IO.FileInfo fi = new System.IO.FileInfo(sourceFile);   
+              //Renomeia o arquivo passando o diretório
+             fi.MoveTo(source+teste);  
+             Console.WriteLine("Arquivo renomeado."); 
+        }
+    }
+}
